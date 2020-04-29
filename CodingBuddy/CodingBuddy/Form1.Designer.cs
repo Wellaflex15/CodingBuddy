9f18f398642869445f9ffe488711060bc9388c48
Rickard Fors <rickard.frs@gmail.com>
2020-04-29 20:15:32 +0200

    test2

--- CodingBuddy/CodingBuddy/Form1.Designer.cs
+++ CodingBuddy/CodingBuddy/Form1.Designer.cs
@@ -36,9 +36,10 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(349, 208);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "CodingBuddy";
+            this.label1.Text = "CodingBuddy1.0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
--- CodingBuddy/CodingBuddy/Form1.cs
+++ CodingBuddy/CodingBuddy/Form1.cs
@@ -16,5 +16,10 @@ namespace CodingBuddy
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
